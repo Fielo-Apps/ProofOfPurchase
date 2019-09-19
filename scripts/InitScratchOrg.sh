6391cbcdee0a22563fbc8d657f21bd9467eba886
Daniel Figueiredo <50217245+DanielFigueiredoFielo@users.noreply.github.com>
2019-09-19 12:19:01 -0300

    Adds FieloPRPAdmin permission set

--- scripts/InitScratchOrg.sh
+++ scripts/InitScratchOrg.sh
@@ -3,3 +3,6 @@ sfdx force:package:install --package 04t2J000000MBdn
 
 # Push Source
 sfdx force:source:push
+
+# Assign the permission set to the user
+sfdx force:user:permset:assign --permsetname FieloPRPAdmin
